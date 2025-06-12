f41ebbf6abbdaafe2add3e05b260811d9a956ce4
Sam Oehlert <sam.oehlert@gmail.com>
2025-06-12 17:33:27 -0500

    fix(settings): missing staticfiles_dirs

--- KitchenClip/settings.py
+++ KitchenClip/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ]
 
 
 # Default primary key field type
