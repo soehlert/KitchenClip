f47cae0442a8e120d74257e508993533c105be84
Sam Oehlert <sam.oehlert@gmail.com>
2025-06-12 17:11:15 -0500

    fix(typo): used wrong app name

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 housetracker.wsgi:application
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 KitchenClip.wsgi:application
